7cc55af54a879ccb7a9dca623e5d69ca1dc4b6b9
Md. Nobir Hasan <nobir.wd@gmail.com>
2025-08-26 07:25:05 +0600

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
     }
     public function home()
     {
-        if(!auth()->user()->is_approved){
+        if(!auth()->user()?->is_approved){
            return view('errors.notapproved');
         }
         $n['wallet'] = Wallet::where('user_id',auth()->user()->id)->where('status',1)->latest()->get();
